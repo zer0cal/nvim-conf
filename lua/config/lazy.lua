39139f2190d44ef25f838281f85851ef7028de14
Wojciech Falba <wojciech.falba@gmail.com>
2026-02-13 12:21:24 +0100

    add colorschemes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,6 @@ require("lazy").setup({
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "rose-pine" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
 })
--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -10,6 +10,6 @@ return {
 				transparency = true,
 			},
 		})
-		vim.cmd.colorscheme("rose-pine")
+		-- vim.cmd.colorscheme("rose-pine")
 	end,
 }
