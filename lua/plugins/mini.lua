8d26bfb50c7f811c578acee98db2b0d7b2516601
Wojciech Falba <wojciech.falba@gmail.com>
2026-03-10 14:48:40 +0100

    lualine

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,12 +3,12 @@ return { -- Collection of various small independent plugins/modules
 	config = function()
 		require("mini.ai").setup({ n_lines = 500 })
 		require("mini.surround").setup()
-		local statusline = require("mini.statusline")
-		statusline.setup({ use_icons = vim.g.have_nerd_font })
+		-- local statusline = require("mini.statusline")
+		-- statusline.setup({ use_icons = vim.g.have_nerd_font })
 
 		---@diagnostic disable-next-line: duplicate-set-field
-		statusline.section_location = function()
-			return "%2l:%-2v"
-		end
+		-- statusline.section_location = function()
+		-- 	return "%2l:%-2v"
+		-- end
 	end,
 }
