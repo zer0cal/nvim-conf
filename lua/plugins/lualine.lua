d105f63153a1041b2d76237a00e653d0169bceba
Wojciech Falba <wojciech.falba@gmail.com>
2026-03-10 15:24:23 +0100

    lualine - customization

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,6 +5,8 @@ return {
 		require("lualine").setup({
 			options = {
 				icons_enabled = false,
+				component_separators = { left = "░▒░", right = "░" },
+				section_separators = { left = "▓▒░", right = "░▒▓" },
 			},
 		})
 	end,
