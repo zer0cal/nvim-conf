5240b0e6cabfcf2dc5851ee92a7b67c2028b0e86
Wojciech Falba <wojciech.falba@gmail.com>
2026-03-13 12:24:26 +0100

    treesitter: disable indentation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -26,11 +26,11 @@ return { -- Highlight, edit, and navigate code
 
 				-- enables treesitter based folds
 				-- for more info on folds see `:help folds`
-				-- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-				-- vim.wo.foldmethod = 'expr'
+				-- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+				-- vim.wo.foldmethod = "expr"
 
 				-- enables treesitter based indentation
-				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				-- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 			end,
 		})
 	end,
