49447719418c51d148c6f49cd892e128738b86db
Wojciech Falba <wojciech.falba@gmail.com>
2026-03-18 19:45:53 +0100

    lualine transparent bg

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,8 +2,13 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		local auto_theme_custom = require("lualine.themes.auto")
+		auto_theme_custom.normal.c.bg = "none"
+		auto_theme_custom.insert.c.bg = "none"
+		auto_theme_custom.visual.c.bg = "none"
 		require("lualine").setup({
 			options = {
+				theme = auto_theme_custom,
 				icons_enabled = false,
 				component_separators = { left = "│", right = "│" },
 				section_separators = { left = "▓▒░", right = "░▒▓" },
