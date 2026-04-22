a9eda9204592d8b212ec6fed8804f8d5e4d27b88
Wojciech Falba <wojciech.falba@gmail.com>
2026-04-22 19:05:12 +0200

    fix: mulitcursor keybid

--- lua/plugins/mulicursor.lua
+++ lua/plugins/mulicursor.lua
@@ -22,13 +22,13 @@ return {
 		end)
 
 		-- Add or skip adding a new cursor by matching word/selection
-		set({ "n", "x" }, "<leader>n", function()
+		set({ "n", "x" }, "<leader>m", function()
 			mc.matchAddCursor(1)
 		end)
 		set({ "n", "x" }, "<leader>s", function()
 			mc.matchSkipCursor(1)
 		end)
-		set({ "n", "x" }, "<leader>N", function()
+		set({ "n", "x" }, "<leader>M", function()
 			mc.matchAddCursor(-1)
 		end)
 		set({ "n", "x" }, "<leader>S", function()
